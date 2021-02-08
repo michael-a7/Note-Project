9ec60c3d740e8334912afb442fc8bf988b5a2d33
michael-a7 <74381482+michael-a7@users.noreply.github.com>
2021-02-08 02:04:55 -0800

    Update App.js

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import Stylesheet from './Styling'
 import Home from "./Home.js"
 import ClassComp from "./ClassComp"
 import {Route} from "react-router-dom";
+import Test from "./Test";
 function App() {
     return(
         <div>
             <Route exact path="/" component={Home} />
             <Route exact path="/classcomp" component={ClassComp} />
+            <Route exact path="/test" component={Test} />
         </div>
     )
 } 
