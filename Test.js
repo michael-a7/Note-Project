24132985aa5306a3f9d113a536d0e8ea1a6577d9
michael-a7 <74381482+michael-a7@users.noreply.github.com>
2021-02-08 02:11:38 -0800

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,19 +1,36 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom'
 import ls from 'local-storage'
 import Modal from 'react-modal';
 function Test(){
     const [text,newText]=useState("")
+    const [fileName, myFile] = useState("")
+    const [Open, setWindow] = useState(false)
     function handleChange(event){
         newText(event.target.value);
     }
-    function handleSubmit(event){
-        localStorage.setItem('Page',text)
+    function handleFileChange(event){
+        myFile(event.target.value);
     }
-    function handleLoad(event){
-        //Issue
-        const data = localStorage.getItem('Page')
+    function handleModal(event){
+        setWindow(!Open);
+    }
+    function handleSubmit(event){
+        const data = [fileName, text]
+        localStorage.setItem(fileName,JSON.stringify(data))
+    }
+    function handleLoading(event){
+        //WORK IN PROGRESS
+        const item = JSON.parse(localStorage.getItem(fileName));
+        newText("TextA")
+        myFile("TextB")
+        setWindow(!Open);
+        //WORK IN PROGRESS
 
     }
+
+
+
     return(
         <div>
             <form>
@@ -25,8 +42,27 @@ function Test(){
                         cols="150"
                     />
                 </form>
-                <button className="click1" onClick={handleSubmit}> Save</button>
-                <button className="click2"onClick={handleLoad}> Load</button>
+                <button
+                    className="click1"
+                    onClick={handleModal}
+                >
+                        Save/Load
+                </button>
+                <Modal
+                isOpen = {Open}
+                onRequestClose = {handleModal}
+                className="modal">
+                    <div className="modalContent">
+                        <form>
+                            <input type ="text" placeholder="File Name" value={fileName} onChange={handleFileChange}/>
+                        </form>
+                        <button className="click2" onClick={handleSubmit}>Save</button>
+                        <button className="click1" onClick={handleLoading}> Load</button>
+                        <button className="click2" onClick = {handleModal}>Close</button>
+                        </div>
+                </Modal>
+                <button className="click2"><Link to="/" className="buttonText">Home</Link></button>
+                <h1>{fileName}</h1>
         </div>
     )
 }
