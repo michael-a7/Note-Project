9753d2b25ea755163d9a2e3932b824e15f84f4da
Michael Albo <michael.albo1@yahoo.com>
2021-02-16 18:07:13 -0800

    home page retooling

--- src/App/Boxes.js
+++ src/App/Boxes.js
@@ -1,10 +1,21 @@
 import {React,useState} from 'react';
+import {Link} from 'react-router-dom';
 function Boxes(){
+    const saveData = JSON.parse(localStorage.getItem("Save List"))
     return(
         <div>
-            <div className="savedNote">
-                <h3>Saved Note</h3>
-                <p>Sample</p>
+            <div className="displayNotes">
+                <Link to="/notes" className = "buttonText">
+                    <div className = "newNote">
+                        <h3 style={{color:"black"}}>New Note</h3>
+                    </div>
+                </Link>
+                {saveData.map(note=>(
+                        <div className="savedNote">
+                            <h3 className="noteHeader">{note.fileName}</h3>
+                            <p>{note.text}</p>
+                        </div>
+                    ))}
             </div>
         </div>
     )
--- src/App/Home.js
+++ src/App/Home.js
@@ -5,37 +5,9 @@ import ClassComp from '../ClassComp'
 import Modal from 'react-modal'
 import Boxes from './Boxes'
 export default function Home(){
-    const [open, setOpen] = useState(false)
-    const [fileName, myFile] = useState("")
-    function handleModal(){
-        setOpen(!open);
-    }
-    function handleFileChange(event){
-        myFile(event.target.value);
-    }
     return(
         <div>
            <h1 id="head">Welcome to NoteHolder</h1>
-            <button className="click1">
-            <Link to="/notes" className="buttonText">
-                   New Note
-               </Link>
-            </button>
-            <Modal
-                isOpen = {open}
-                onRequestClose = {handleModal}
-                className="modal">
-                    <file>
-                    <input type ="text" placeholder="File Name" value={fileName} onChange={handleFileChange}/>
-                    </file>
-                    <div>
-                        <button className="click1">Go</button>
-                        <button className="click2" onClick = {handleModal}>Close</button>
-                    </div>
-                </Modal>
-            <button className="click2" onClick={handleModal}>
-                Load
-            </button>
             <Boxes />
         </div>
     )
